b1cce18c95dbb96ae85eb1c724769bd389872902
peteparkinson <43157092+peteparkinson@users.noreply.github.com>
2018-10-26 12:33:53 -0400

    removed redundancies, added black and white

--- colors.js
+++ colors.js
@@ -19,146 +19,102 @@
  */
 function getColor(letter, level){
     let colors = [];
-    /*
-    if(level === retro){
+
+    //0,
+    //1: offwhite
+    //2: light
+    //3: dark
+    //4: black
+    //5: white
+
+
+    if(level % 10 === 0){ //white-blue, light-blue, dark-blue
         colors = [
             null,
-            '#00FFFF',  //I
-            '#0000CD',  //J
-            '#FF4500',  //L
-            '#FFD700',  //O
-            '#00FF00',  //S
-            '#9400D3',  //T
-            '#FF0000',  //Z
-        ];
-    }else 
-    */if(level % 10 === 0){ //white-blue, light-blue, dark-blue
-        colors = [
-            null,
-            '#E0E0FF',  //I
-            '#0057F6',  //J
-            '#3EBEFF',  //L
-            '#E0E0FF',  //O
-            '#0057F6',  //S
-            '#E0E0FF',  //T
-            '#3EBEFF',  //Z
+            '#E0E0FF',  //off-white
+            '#3EBEFF',  //light
+            '#0057F6',  //dark
             '#000000',  //black
             '#FFFFFF',  //white
         ];
     }else if(level % 10 === 1){  //white-green, light-green, dark-green
         colors = [
             null,
-            '#E0FFE0',  //I
-            '#00A800',  //J
-            '#80D010',  //L
-            '#E0FFE0',  //O
-            '#00A800',  //S
-            '#E0FFE0',  //T
-            '#80D010',  //Z
+            '#E0FFE0',  //off-white
+            '#80D010',  //light
+            '#00A800',  //dark
             '#000000',  //black
             '#FFFFFF',  //white
         ];
     }else if(level % 10 === 2){  //white, light-pink, pink
         colors = [
             null,
-            '#E0FFFF',  //I
-            '#D800CD',  //J
-            '#F878F8',  //L
-            '#E0FFFF',  //O
-            '#D800CD',  //S
-            '#E0FFFF',  //T
-            '#F878F8',  //Z
+            '#E0FFFF',  //off-white
+            '#F878F8',  //light
+            '#D800CD',  //dark
             '#000000',  //black
             '#FFFFFF',  //white
         ];
     }else if(level % 10 === 3){  //white-blue, green-teal, dark-blue-teal
         colors = [
             null,
-            '#FFFFE0',  //I
-            '#0058F8',  //J
-            '#5BDB57',  //L
-            '#FFFFE0',  //O
-            '#0058F8',  //S
-            '#FFFFE0',  //T
-            '#5BDB57',  //Z
+            '#FFFFE0',  //off-white
+            '#5BDB57',  //light
+            '#0058F8',  //dark
             '#000000',  //black
             '#FFFFFF',  //white
         ];
     }else if(level % 10 === 4){  //white-pink, teal, dark-pink
         colors = [
             null,
-            '#F17EAB',  //I
-            '#E7005B',  //J
-            '#58F898',  //L
-            '#F17EAB',  //O
-            '#E7005B',  //S
-            '#F17EAB',  //T
-            '#58F898',  //Z
+            '#F17EAB',  //off-white
+            '#58F898',  //light
+            '#E7005B',  //dark
             '#000000',  //black
             '#FFFFFF',  //white
         ];
     }else if(level % 10 === 5){  //white-green, lavender, green-teal
         colors = [
             null,
-            '#AAFACA',  //I
-            '#58F898',  //J
-            '#6B88FF',  //L
-            '#AAFACA',  //O
-            '#58F898',  //S
-            '#AAFACA',  //T
-            '#6B88FF',  //Z
+            '#AAFACA',  //off-white
+            '#6B88FF',  //light
+            '#58F898',  //dark
             '#000000',  //black
             '#FFFFFF',  //white
         ];
     }else if(level % 10 === 6){  //white-orange, gray, dark-orange
         colors = [
             null,
-            '#E0E0FF',  //I
-            '#F83800',  //J
-            '#7F7F7F',  //L
-            '#E0E0FF',  //O
-            '#F83800',  //S
-            '#E0E0FF',  //T
-            '#7F7F7F',  //Z
+            '#E0E0FF',  //off-white
+            '#7F7F7F',  //light
+            '#F83800',  //dark
             '#000000',  //black
             '#FFFFFF',  //white
         ];
     }else if(level % 10 === 7){  //white-purple, maroon, purple
         colors = [
             null,
-            '#E0FFE0',  //I
-            '#AB0023',  //J
-            '#6B47FF',  //L
-            '#E0FFE0',  //O
-            '#AB0023',  //S
-            '#E0FFE0',  //T
-            '#6B47FF',  //Z
+            '#E0FFE0',  //off-white
+            '#6B47FF',  //light
+            '#AB0023',  //dark
             '#000000',  //black
             '#FFFFFF',  //white
         ];
     }else if(level % 10 === 8){  //white-blue, orange, blue
         colors = [
             null,
-            '#E0E0FF',  //I
-            '#0058F8',  //J
-            '#F83800',  //L
-            '#E0E0FF',  //O
-            '#0058F8',  //S
-            '#E0E0FF',  //T
-            '#F83800',  //Z
+            '#E0E0FF',  //off-white
+            '#F83800',  //light
+            '#0058F8',  //dark
             '#000000',  //black
             '#FFFFFF',  //white
         ];
     }else if(level % 10 === 9){  //white-orange, light-orange, orange
         colors = [
             null,
-            '#E0FFFF',  //I
-            '#F83800',  //J
-            '#FFA347',  //L
-            '#E0FFFF',  //O
-            '#F83800',  //S
-            '#E0FFFF',  //T
-            '#FFA347',  //Z
+            '#E0FFFF',  //off-white
+            '#FFA347',  //light
+            '#F83800',  //dark
             '#000000',  //black
             '#FFFFFF',  //white
         ];
